7ce102c32848130c8d0bcbb33e94e8db7da54b68
Sven Borden <glaglasven@live.com>
2022-09-06 21:03:42 +0200

    Created account data model

--- magic_pass_challenge/lib/main.dart
+++ magic_pass_challenge/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Magic pass challenge',
       theme: ThemeData(
         // This is the theme of your application.
         //
---
+++ magic_pass_challenge/lib/src/models/account.dart
@@ -0,0 +1,12 @@
+class Account {
+  final String id;
+  final String magicPassNumber;
+
+  final String password;
+
+  Account({
+    required this.id,
+    required this.password,
+    required this.magicPassNumber
+  });
+}
