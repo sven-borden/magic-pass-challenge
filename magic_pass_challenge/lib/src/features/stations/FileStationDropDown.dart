0ba7a011dec368ae147442d3768245a65aa2118d
Sven Borden <glaglasven@live.com>
2022-09-27 22:48:50 +0200

    Corrected library json file

--- magic_pass_challenge/lib/src/features/stations/FileStationDropDown.dart
+++ magic_pass_challenge/lib/src/features/stations/FileStationDropDown.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class FileStationDropDown extends StatefulWidget {
@@ -8,34 +10,40 @@ class FileStationDropDown extends StatefulWidget {
   State<FileStationDropDown> createState() => _FileStationDropDownState();
 }
 
-const List<String> stationList = <String>['One', 'Two', 'Three', 'Four'];
 
 class _FileStationDropDownState extends State<FileStationDropDown> {
-  String dropdownValue = stationList.first;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.blue),
-      underline: Container(
-        height: 2,
-        color: Colors.blueAccent,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: stationList.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
+    return Center(
+      child: FutureBuilder(
+          builder: (context, snapshot) {
+            var stationList = json.decode(snapshot.data.toString());
+            String dropdownValue = stationList.first;
+            return DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              elevation: 16,
+              style: const TextStyle(color: Colors.blue),
+              underline: Container(
+                height: 2,
+                color: Colors.blueAccent,
+              ),
+              onChanged: (String? value) {
+                // This is called when the user selects an item.
+                setState(() {
+                  dropdownValue = value!;
+                });
+              },
+              items: stationList.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            );
+          }
+      )
     );
   }
 }
